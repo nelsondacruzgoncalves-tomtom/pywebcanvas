e9a1aca34b3d47153a2b5773a5a387103815464f
Isaac Beverly <imbev@protonmail.com>
2022-05-14 17:49:42 -0500

    Add docstrings

--- pywebcanvas/background.py
+++ pywebcanvas/background.py
@@ -4,10 +4,21 @@ import pywebcanvas as pwc
 
 
 class Background:
+    """
+    A class used to interact with the background of the canvas.
+    """
     def __init__(self, canvas):
         self.canvas = canvas
 
     def fill(self, color):
+        """
+        Set the background color of the canvas.
+        Parameters
+        ----------
+        color : str
+                The color to change the background to. Can be in hexadecimal 
+                format or most english color names.
+        """
         hex_color = Color(color).hex      
         pwc.log(f"Set {self.canvas} background to {hex_color}")
         self.canvas.canvas.setAttribute('style', f'background-color:{hex_color}')
