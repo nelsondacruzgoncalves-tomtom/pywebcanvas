bae96fc5a5af96ffde301c52bf06022cf2430899
Isaac Beverly <imbev@protonmail.com>
2022-08-11 19:16:03 +0000

    Add Path.bezier_curve_to method

--- pywebcanvas/path.py
+++ pywebcanvas/path.py
@@ -20,6 +20,10 @@ class Path:
     def line_to(self, x_pos, y_pos, color=""):
         pwc.log(f"Path {self} line to {x_pos}, {y_pos}")
         self.queue.append(("line_to", (x_pos, y_pos)))
+    
+    def bezier_curve_to(self, c1_x_pos, c1_y_pos, c2_x_pos, c2_y_pos, x_pos, y_pos):
+        pwc.log(f"Path {self} bezier curve to {x_pos}, {y_pos} with control points ({c1_x_pos}, {c1_y_pos}), ({c2_x_pos}, {c2_y_pos})")
+        self.queue.append(("bezier_curve_to", (c1_x_pos, c1_y_pos, c2_x_pos, c2_y_pos, x_pos, y_pos)))
 
     def fill(self, color=""):
         pwc.log(f"Fill Path {self}")
@@ -39,6 +43,8 @@ class Path:
                 ctx.moveTo(*action[1])
             elif action[0] == "line_to":
                 ctx.lineTo(*action[1])
+            elif action[0] == "bezier_curve_to":
+                ctx.bezierCurveTo(*action[1])
             elif action[0] == "fill":
                 if not action[1] == "":
                     hex_color = Color(action[1]).hex 
